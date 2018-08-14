2d048341dc8aa079ffe592b3af4740439b7fa837
achalupka <amymch@gmail.com>
2018-08-14 12:28:08 -0400

    Add files via upload

--- 5.5/init.sh
+++ 5.5/init.sh
@@ -5,7 +5,7 @@ cat << EOF > $tfile
 CREATE DATABASE IF NOT EXISTS \`mysql\` CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE mysql;
 FLUSH PRIVILEGES;
-CREATE USER 'root'@'%' IDENTIFIED BY '$password';
+CREATE USER 'root'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
